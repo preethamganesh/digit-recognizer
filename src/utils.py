b2f4336cf127a04ac68c9d6a9369716ba0cc5d25
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2023-10-13 21:16:46 -0400

    [#1] added function load_json_file().
    Loads a JSON file as a dictionary.

--- src/utils.py
+++ src/utils.py
@@ -128,3 +128,37 @@ def save_json_file(
         json.dump(dictionary, out_file, indent=4)
     out_file.close()
     add_to_log("{} file saved successfully.".format(file_name))
+
+
+def load_json_file(file_name: str, directory_path: str) -> Dict[Any, Any]:
+    """Loads a JSON file as a dictionary.
+
+    Loads a JSON file as a dictionary into memory based on the file_name.
+
+    Args:
+        file_name: A string for the name of the of the file to be loaded.
+        directory_path: A string for the location where the directory path exists.
+
+    Returns:
+        A dictionary loaded from the JSON file.
+
+    Exceptions:
+        FileNotFoundError: If file path does not exist, then this error occurs.
+    """
+    # Types checks input arguments.
+    assert isinstance(file_name, str), "Variable file_name should be of type 'str'."
+    assert isinstance(
+        directory_path, str
+    ), "Variable directory_path should be of type 'str'."
+
+    file_path = "{}/{}.json".format(directory_path, file_name)
+
+    # Loads the JSON file as dictionary from the file location.
+    try:
+        with open(file_path, "r") as out_file:
+            dictionary = json.load(out_file)
+        out_file.close()
+        return dictionary
+
+    except FileNotFoundError:
+        raise FileNotFoundError("File path {} does not exist.".format(file_path))
