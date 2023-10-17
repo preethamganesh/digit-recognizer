3069213296a0b63cc4d39700a50c68785b6e7288
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2023-10-16 20:06:23 -0400

    [#3] added __init__() function to Dataset class.
    Creates object attributes for the Dataset class.

---
+++ src/digit_recognizer/dataset.py
@@ -0,0 +1,24 @@
+from typing import Dict, Any
+
+
+class Dataset(object):
+    """Loads the dataset based on model configuration."""
+
+    def __init__(self, model_configuration: Dict[str, Any]) -> None:
+        """Creates object attributes for the Dataset class.
+
+        Creates object attributes for the Dataset class.
+
+        Args:
+            model_configuration: A dictionary for the configuration of model's current version.
+
+        Returns:
+            None.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(
+            model_configuration, dict
+        ), "Variable model_configuration should be of type 'dict'."
+
+        # Initalizes class variables.
+        self.model_configuration = model_configuration
