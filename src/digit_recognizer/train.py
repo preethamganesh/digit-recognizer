100f14e0b50c95fe305e8e0cb068c9f17d47621f
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2023-10-17 17:34:14 -0400

    [#3] added __init__() function to Train class.
    Creates object attributes for the Train class.

---
+++ src/digit_recognizer/train.py
@@ -0,0 +1,20 @@
+class Train(object):
+    """Trains the digit recognition model based on the configuration."""
+
+    def __init__(self, model_version: str) -> None:
+        """Creates object attributes for the Train class.
+
+        Creates object attributes for the Train class.
+
+        Args:
+            model_version: A string for the version of the current model.
+
+        Returns:
+            None.
+        """
+        # Asserts type & value of the arguments.
+        assert isinstance(model_version, str), "Variable model_version of type 'str'."
+
+        # Initalizes class variables.
+        self.model_version = model_version
+        self.best_validation_loss = None
