5ebb43474c4fb1fc6618f0ffc62ac03a8aec1dbe
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2023-10-28 20:11:38 -0400

    [#3] added load_dataset() function to Train class.
    Loads dataset based on model configuration.

--- src/digit_recognizer/train.py
+++ src/digit_recognizer/train.py
@@ -13,6 +13,8 @@ logging.getLogger("tensorflow").setLevel(logging.FATAL)
 
 
 from src.utils import load_json_file
+from src.digit_recognizer.dataset import Dataset
+from src.utils import add_to_log
 
 
 class Train(object):
@@ -54,3 +56,69 @@ class Train(object):
         self.model_configuration = load_json_file(
             "v{}".format(self.model_version), model_configuration_directory_path
         )
+
+    def load_dataset(self) -> None:
+        """Loads dataset based on model configuration.
+
+        Loads dataset based on model configuration.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        # Initializes object for the Dataset class.
+        self.dataset = Dataset(self.model_configuration)
+
+        # Loads original train & test CSV files as dataframes.
+        self.dataset.load_data()
+        add_to_log(
+            "No. of examples in the original train data: {}".format(
+                len(self.dataset.original_train_data)
+            )
+        )
+        add_to_log(
+            "No. of examples in the original test data: {}".format(
+                len(self.dataset.original_test_data)
+            )
+        )
+        add_to_log("")
+
+        # Splits original train data into new train, validation & test data.
+        self.dataset.split_dataset()
+        add_to_log(
+            "No. of examples in the new train data: {}".format(
+                self.dataset.n_train_examples
+            )
+        )
+        add_to_log(
+            "No. of examples in the new validation data: {}".format(
+                self.dataset.n_validation_examples
+            )
+        )
+        add_to_log(
+            "No. of examples in the new test data: {}".format(
+                self.dataset.n_test_examples
+            )
+        )
+        add_to_log("")
+
+        # Converts split data tensorflow dataset and slices them based on batch size.
+        self.dataset.shuffle_slice_dataset()
+        add_to_log(
+            "No. of train steps per epoch: {}".format(
+                self.dataset.n_train_steps_per_epoch
+            )
+        )
+        add_to_log(
+            "No. of validation steps per epoch: {}".format(
+                self.dataset.n_validation_steps_per_epoch
+            )
+        )
+        add_to_log(
+            "No. of test steps per epoch: {}".format(
+                self.dataset.n_test_steps_per_epoch
+            )
+        )
+        add_to_log("")
