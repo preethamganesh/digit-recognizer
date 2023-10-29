a8a539aa24b3a769339996aa1d7ad311a5b75e6c
Preetham Ganesh <preetham.ganesh2021@gmail.com>
2023-10-28 20:06:37 -0400

    [#3] added load_model_configuration() function to Train class.
    Loads the model configuration file for current version.

--- src/digit_recognizer/train.py
+++ src/digit_recognizer/train.py
@@ -1,3 +1,20 @@
+import os
+import sys
+import warnings
+import argparse
+import logging
+
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+BASE_PATH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(BASE_PATH)
+warnings.filterwarnings("ignore")
+logging.getLogger("tensorflow").setLevel(logging.FATAL)
+
+
+from src.utils import load_json_file
+
+
 class Train(object):
     """Trains the digit recognition model based on the configuration."""
 
@@ -18,3 +35,22 @@ class Train(object):
         # Initalizes class variables.
         self.model_version = model_version
         self.best_validation_loss = None
+
+    def load_model_configuration(self) -> None:
+        """Loads the model configuration file for current version.
+
+        Loads the model configuration file for current version.
+
+        Args:
+            None.
+
+        Returns:
+            None.
+        """
+        self.home_directory_path = os.getcwd()
+        model_configuration_directory_path = (
+            "{}/configs/models/digit_recognizer".format(self.home_directory_path)
+        )
+        self.model_configuration = load_json_file(
+            "v{}".format(self.model_version), model_configuration_directory_path
+        )
